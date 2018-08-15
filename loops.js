ae2acc343b40e1ce4a49eb7054e80fccbaa2ac6d
christopherbroussard44@yahoo.com <christopherbroussard44@yahoo.com>
2018-08-15 22:07:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,9 +3,8 @@ function forLoop(array) {
   array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   return array;
 }
-function whileLoop(countdown) 
-  countdown = 100
-  while ([countdown > 0]) {
+function whileLoop(n) 
+  while ([n > 0]) {
     console.log(--countdown);
     
   }
