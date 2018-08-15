680ef4b6768d5031bfee5ba8d029a76a75785a06
christopherbroussard44@yahoo.com <christopherbroussard44@yahoo.com>
2018-08-15 21:42:38 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array) {
  const t = 25;
   for (let i = 0; i < t; i++) {
-   array.push("I am a strange loop.");
+   array.push("I am ${1} strange loop.");
   }
   return array;
 }
