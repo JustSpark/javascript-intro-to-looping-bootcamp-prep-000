d3bbddfea51371c99dbf6d0bc043ecca8b065eeb
christopherbroussard44@yahoo.com <christopherbroussard44@yahoo.com>
2018-08-15 22:23:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,7 +10,7 @@ function forLoop() {
 }
 function whileLoop() {
   let countdown = 100;
-  while (countdown > 0) {
+  while (countdown > 10) {
     console.log(countdown);
     countdown--;
     return 'done';
