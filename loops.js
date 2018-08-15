c5142a0a345a106b59255868670f23d6a99f2b3d
christopherbroussard44@yahoo.com <christopherbroussard44@yahoo.com>
2018-08-15 22:18:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,3 +8,9 @@ function forLoop() {
 
   return [array, t];
 }
+function maybeTrue() {
+  return Math.random() >= 0.5;
+  while (maybeTrue()) {
+    console.log("And I ran; I ran so far away.")
+  }
+}
